936d042838575aac8be1d5a8bb44dee4f40422ea
Sayed1-11 <157264560+Sayed1-11@users.noreply.github.com>
2024-01-23 20:56:56 +0600

    Update views.py

--- account/views.py
+++ account/views.py
@@ -30,7 +30,7 @@ class Registrationview(FormView):
         token = default_token_generator.make_token(user)
         uid = urlsafe_base64_encode(force_bytes(user.pk))
         print("uid ",uid)
-        confirm_link = f"http://127.0.0.1:8000/active/{uid}/{token}"
+        confirm_link = f"https://careerswift.onrender.com/active/{uid}/{token}"
         email_subject = "Confirm Email"
         email_body = render_to_string('confirm_email.html',{'confirm_link': confirm_link})
         email = EmailMultiAlternatives(email_subject, "",to=[user.email])
